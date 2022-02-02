8ae764335c518d73f938061588d2d5648177fa1f
rafaelbombonatto <rafael.bombonatto@gmail.com>
2022-02-02 08:52:16 -0300

    Alterado o projeto de NETFLIX para MELTAFLIX. Realizados ajustes no layout e carrossel de videoclips

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,14 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+    items:1,
+    merge:true,
     loop:true,
     margin:10,
     nav:true,
+    video:true,
+    lazyLoad:true,
+    center:true,
     responsive:{
         0:{
             items:1
@@ -14,3 +21,12 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+
